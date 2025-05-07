6af6b8cfd13e98b3b0bd7c68578663546498e3c6
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-05-07 17:30:04 -0300

    Advancing Letters

---
+++ beecrowd/bee1607.cpp
@@ -0,0 +1,28 @@
+//tentar reenviar pq o site do beecrowd ta sem fucnionar???
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int t;cin>>t;
+    while(t--){
+        string a,b;
+        cin>>a>>b;
+        int cont=0;
+
+        for(int i=0;i<a.size();i++){
+            if(a[i]>b[i]){
+                cont++;
+                cont+=('z'-a[i]);
+                cont+=(b[i]-'a');
+            }else{
+                cont+=(b[i]-a[i]);
+            }
+        }
+
+        cout<<cont<<endl;
+    }
+
+    return 0;
+}
