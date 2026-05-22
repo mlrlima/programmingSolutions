071b8d76b56f0d01e2072011635f07b20a50fca6
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2026-05-22 11:23:39 -0300

    longestCommonPrefix.cpp

---
+++ leetcode/longestCommonPrefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix="";
+        for(int i=0;i<strs[0].size();i++){
+            char c=strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]!=c) return prefix;
+            }
+            prefix+=c;
+        }
+        return prefix;
+    }
+};
