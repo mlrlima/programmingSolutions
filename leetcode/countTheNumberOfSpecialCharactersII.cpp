5f606619f5af2ff560c45a0c1bc915860d220536
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2026-05-26 10:15:42 -0300

    countTheNumberOfSpecialCharactersII.cpp

---
+++ leetcode/countTheNumberOfSpecialCharactersII.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        bool lower[26]={};
+        bool upper[26]={};
+        bool b4[26]={};
+
+        for(int i=0;i<word.size();i++){
+            if(word[i]>='a'){
+                lower[word[i]-'a']=true;
+                if(upper[word[i]-'a']==true) b4[word[i]-'a']=true;
+            }
+            else upper[word[i]-'A']=true;
+        }
+
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(upper[i] && lower[i] && !b4[i]) count++;
+        }
+
+        return count;
+    }
+};
