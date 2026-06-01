6dd6b221e3a5798a4a16745d14c404fcb63bb673
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2026-06-01 13:44:30 -0300

    addTwoNumbers.java

---
+++ leetcode/addTwoNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int resto=0;
+        ListNode first=new ListNode();
+        ListNode i=first;
+        
+        while(true){
+            int sum=l1.val+l2.val+resto;
+            resto=sum/10;
+            sum%=10;
+
+            i.val=sum;
+            if(l1.next==null && l2.next==null && resto==0){
+                break;
+            }
+
+            ListNode next=new ListNode();
+            i.next=next;
+            i=next;
+
+            if(l1.next==null) l1.val=0;
+            else l1=l1.next;
+
+            if(l2.next==null) l2.val=0;
+            else l2=l2.next;
+        }
+
+        return first;
+
+    }
+}
